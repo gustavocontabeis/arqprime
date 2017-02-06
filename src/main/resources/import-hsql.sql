4d9e6885bf6a8119b3fc83454125e310a7deb69d
Gustavo da Silva f768557 <gustavo.silva@tty2000.com.br>
2017-02-06 19:38:37 -0200

    atualiza gitignore

---
+++ src/main/resources/import-hsql.sql
@@ -0,0 +1,3 @@
+insert into perfil_acesso (descricao, nome, id_perfil) values ('Administrador de usuários', 'USU-ADM', 1);
+insert into perfil_acesso (descricao, nome, id_perfil) values ('Usuário web', 'WEB', 2);
+insert into perfil_acesso (descricao, nome, id_perfil) values ('Usuário de Teste', 'TST', 3);
